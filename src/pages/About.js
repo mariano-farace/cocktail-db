75ac903cebaf9243da4282263ed03bb4b2cf9eb6
Mariano <mariano_farace@hotmail.com>
2022-11-10 13:12:55 +0100

    Implement about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus omnis
+        commodi cumque ex a facilis atque nemo eveniet corrupti recusandae
+        magni, quasi fugit quisquam. Sint quisquam nostrum cumque dignissimos
+        consequuntur corporis, asperiores ad recusandae aut quis explicabo,
+        optio deserunt quidem.
+      </p>
+    </section>
+  );
 }
 
 export default About;
