91187b45ace3ca990c422ca4773b3a059866d663
Mariano <mariano_farace@hotmail.com>
2022-11-10 13:06:43 +0100

    Implement NavBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCockTail from "./pages/SingleCockTail";
+import Error from "./pages/Error";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/cocktails:id" element={<SingleCockTail />} />
+        <Route exact path="/*" element={<Error />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/SingleCockTail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingleCockTail() {
+  return <div>SingleCockTail</div>;
+}
+
+export default SingleCockTail;
