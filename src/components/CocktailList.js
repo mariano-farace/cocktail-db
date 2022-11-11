484db9c3ecc33228f1eaef7623f1ec885250ea4d
Mariano <mariano_farace@hotmail.com>
2022-11-11 10:50:17 +0100

    create cocktail list

---
+++ src/components/CocktailList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useGlobalContext } from "../context";
+import Loading from "./Loading";
+
+function CocktailList() {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        no cocktails matched your search criteria
+      </h2>
+    );
+  }
+
+  return <div>CocktailList</div>;
+}
+
+export default CocktailList;
