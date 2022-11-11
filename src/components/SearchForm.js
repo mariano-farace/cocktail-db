c5a87b6c4a3f268e57d632c9c4f0ef6b9f78103a
Mariano <mariano_farace@hotmail.com>
2022-11-11 16:53:30 +0100

    Implement search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useGlobalContext } from "../context";
 
 function SearchForm() {
   const { setSearchTerm } = useGlobalContext();
-  return <div>SearchForm</div>;
+  const searchValue = useRef("");
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  //Will set focus on the input. That's one of the reason for selecting ref instead of UseState
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  return (
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          ></input>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
