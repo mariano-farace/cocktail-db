28896ae848d5af7e5efb856ce70b46e768e9a13d
Mariano <mariano_farace@hotmail.com>
2022-11-11 12:01:44 +0100

    set provider

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleCockTail from "./pages/SingleCockTail";
 import Error from "./pages/Error";
+import { AppProvider } from "./context";
+
+//TODO hacer un refactor de app provide y para ponerlo en el index
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/cocktails:id" element={<SingleCockTail />} />
-        <Route exact path="/*" element={<Error />} />
-      </Routes>
+      <AppProvider>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/cocktails:id" element={<SingleCockTail />} />
+          <Route exact path="/*" element={<Error />} />
+        </Routes>
+      </AppProvider>
     </>
   );
 }
