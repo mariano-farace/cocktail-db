fddc8e43ba07d5f226e3ef327af64812c613dbe6
Mariano <mariano_farace@hotmail.com>
2022-11-12 13:10:09 +0100

    Remove todos

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import SingleCockTail from "./pages/SingleCockTail";
 import Error from "./pages/Error";
 import { AppProvider } from "./context";
 
-//TODO hacer un refactor de app provide y para ponerlo en el index
-//TODO ver como sacar el loading undefined del principio
 //TODO ver como funciona la api, para hacer mas fetch y hacer mas render! scrolling infinito
-//TODO implementar la pantalla del loading que no esta andando!!!
 function App() {
   return (
     <>
