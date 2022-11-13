da39c4d411c9eef5490547fd644d33f6e7c28280
Mariano <mariano_farace@hotmail.com>
2022-11-13 11:58:52 +0100

    Migrate to styled components

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,78 @@
 import React from "react";
+import styled from "styled-components";
+
+const Loader = styled.div`
+  background: transparent;
+  -webkit-animation: load1 1s infinite ease-in-out;
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+
+  :before,
+  :after {
+    background: transparent;
+    -webkit-animation: load1 1s infinite ease-in-out;
+    animation: load1 1s infinite ease-in-out;
+    width: 1em;
+    height: 4em;
+  }
+
+  color: var(--primaryColor);
+  text-indent: -9999em;
+  margin: 88px auto;
+  margin-top: 20rem;
+  position: relative;
+  font-size: 3rem;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+
+  :before,
+  :after {
+    position: absolute;
+    top: 0;
+    content: "";
+  }
+
+  :before {
+    left: -1.5em;
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+  :after {
+    left: 1.5em;
+  }
+
+  @-webkit-keyframes load1 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 0;
+      height: 4em;
+    }
+    40% {
+      box-shadow: 0 -2em;
+      height: 5em;
+    }
+  }
+  @keyframes load1 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 0;
+      height: 4em;
+    }
+    40% {
+      box-shadow: 0 -2em;
+      height: 5em;
+    }
+  }
+`;
 
 const Loading = () => {
-  return <div className="loader"></div>;
+  return <Loader />;
 };
 
 export default Loading;
